38a7907f41e565aaf7e84773bee2311ffa3b96c6
shotaro <850123.a@gmail.com>
2025-08-08 00:36:15 +0900

    アコーディオンメニュー実装

---
+++ js/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburgerBtn");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("open");
+  });
+});
+
+$(function () {
+  $("#hamburgerBtn").on("click", function () {
+    $("#accordionMenu").slideToggle(300);
+  });
+
+  $(window).on("resize", function () {
+    if (window.innerWidth > 670) {
+      $("#accordionMenu").hide();
+      $("#hamburgerBtn").removeClass("open");
+    }
+  });
+});
