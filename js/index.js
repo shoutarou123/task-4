c3448454b8fdafa2224d9a566c9af6ec198053a6
shotaro <850123.a@gmail.com>
2025-08-08 14:26:11 +0900

    我が社、サービスセクション作成。スクロール機能追加。

--- js/index.js
+++ js/index.js
@@ -17,4 +17,60 @@ $(function () {
       $("#hamburgerBtn").removeClass("open");
     }
   });
+
+  const btns = [
+    document.getElementById("webBtn"),
+    document.getElementById("seoBtn"),
+    document.getElementById("mobileBtn"),
+  ];
+
+  btns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      btns.forEach((b) => b.classList.remove("active"));
+      btn.classList.add("active");
+    });
+  });
+
+  const HEADER_HEIGHT = 60;
+
+  const scrollMap = [
+    {linkClass: ".about_link", sectionSelector: ".about_content"},
+    {linkClass: ".service_link", sectionSelector: ".service_content"},
+    {linkClass: ".contact_link", sectionSelector: ".contact_content"}
+  ];
+
+  scrollMap.forEach(({linkClass, sectionSelector}) => {
+    const links = document.querySelectorAll(linkClass);
+    const section = document.querySelector(sectionSelector);
+    links.forEach(link => {
+      link.addEventListener("click", function(e) {
+        e.preventDefault();
+        const rect = section.getBoundingClientRect();
+        const offsetTop = window.pageYOffset + rect.top - HEADER_HEIGHT;
+        window.scrollTo({ top: offsetTop, behavior: "smooth"});
+      });
+    });
+  });
+
+  const MobileScrollMap = [
+    {linkId: "accordionAboutLink", sectionSelector: ".about_content"},
+    {linkId: "accordionServiceLink", sectionSelector: ".service_content"},
+    {linkId: "accordionContactLink", sectionSelector: ".contact_content"}
+  ];
+
+  const ACCORDION_HEADER_HEIGHT = 120;
+
+  MobileScrollMap.forEach(({linkId, sectionSelector}) => {
+    const ids = document.getElementById(linkId);
+    const section = document.querySelector(sectionSelector);
+    ids.forEach(id => {
+      id.addEventListener("click", function(e) {
+        e.preventDefault();
+        const rect = section.getBoundingClientRect();
+        const offsetTop = window.pageYOffset + rect.top - ACCORDION_HEADER_HEIGHT;
+        window.scrollTo({ top: offsetTop, behavior: "smooth"});
+      });
+    });
+  });
+
 });
